3149e676d02a181865ad4592f051eb7f10903208
Nikhil Dixit <nvdixit@ncsu.edu>
2021-07-18 15:47:34 -0500

    Began implementing main API functionality

--- main.py
+++ main.py
@@ -2,16 +2,51 @@ from datetime import datetime
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-# Gets the current time
-now = datetime.now()
+all_times = {
+    "GMT": "",
+    "ECT": "",
+    "EET": "",
+    "EAT": "",
+    "NET": "",
+    "PLT": "",
+    "BST": "",
+    "VST": "",
+    "CTT": "",
+    "JST": "",
+    "AET": "",
+    "SST": "",
+    "NST": "",
+    "MIT": "",
+    "HST": "",
+    "AST": "",
+    "PST": "",
+    "PNT": "",
+    "CST": "",
+    "EST": "",
+    "PRT": "",
+    "CNT": "",
+    "AGT": "",
+    "GST": "",
+    "CAT": ""
+}
 
-current_time_24 = now.strftime("%H:%M:%S")
-current_time_12 = now.strftime("%I:%M:%S %p")
+
+@app.route("/twenty_four_times")
+def twenty_four_times():
+    now = datetime.now()
+    current_time_24 = now.strftime("%H:%M:%S")
+    all_times["EST"] = current_time_24
+
+    return jsonify(all_times)
 
 
-@app.route("/")
-def dummy_return():
-    return jsonify(anything="Hello World")
+@app.route("/twelve_times")
+def twelve_times():
+    now = datetime.now()
+    current_time_12 = now.strftime("%I:%M:%S %p")
+    all_times["EST"] = current_time_12
+
+    return jsonify(all_times)
 
 
 if __name__ == '__main__':
