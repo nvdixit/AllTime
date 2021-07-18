bcff7f7bd7a8d577d990f5e986d491fc5cda2d7a
Nikhil Dixit <nvdixit@ncsu.edu>
2021-07-18 11:39:26 -0500

    Implemented basic Flask functionality

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from datetime import datetime
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+# Gets the current time
+now = datetime.now()
+
+current_time_24 = now.strftime("%H:%M:%S")
+current_time_12 = now.strftime("%I:%M:%S %p")
+
+
+@app.route("/")
+def dummy_return():
+    return jsonify(anything="Hello World")
+
+
+if __name__ == '__main__':
+    app.run()
