19694189868b62ded6073aacbf3ee3cd576a0da2
Nikhil Dixit <nvdixit@ncsu.edu>
2021-07-18 17:29:15 -0500

    Implemented 24 hour time functionality

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from datetime import datetime
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+CONST_EST_GMT_DIFF = 5
+HOUR_COUNT_ONE = 24
+HOUR_COUNT_TWO = 12
+DAYLIGHT_SAVINGS = True
+
 all_times = {
     "GMT": "",
     "ECT": "",
@@ -20,7 +25,7 @@ all_times = {
     "HST": "",
     "AST": "",
     "PST": "",
-    "PNT": "",
+    "MST": "",
     "CST": "",
     "EST": "",
     "PRT": "",
@@ -31,11 +36,31 @@ all_times = {
 }
 
 
+@app.route("/")
+def base_return():
+    return "If you're seeing this message, it means you've got the right URL, but not a valid endpoint. " \
+           "Head back to the docs page to find valid endpoints"
+
+
 @app.route("/twenty_four_times")
 def twenty_four_times():
     now = datetime.now()
     current_time_24 = now.strftime("%H:%M:%S")
-    all_times["EST"] = current_time_24
+
+    gmt_hour = (int(current_time_24.split(":")[0]) + CONST_EST_GMT_DIFF) % HOUR_COUNT_ONE
+    minute = str(current_time_24.split(":")[1])
+    second = str(current_time_24.split(":")[2])
+
+    count = 0
+    for key in all_times:
+        final_time = str((gmt_hour + count) % HOUR_COUNT_ONE) + ":" + minute + ":" + second
+
+        if DAYLIGHT_SAVINGS:
+            if key.__eq__("PST") | key.__eq__("MST") | key.__eq__("CST") | key.__eq__("EST"):
+                final_time = str((gmt_hour + count + 1) % HOUR_COUNT_ONE) + ":" + minute + ":" + second
+
+        all_times[key] = final_time
+        count += 1
 
     return jsonify(all_times)
 
@@ -43,8 +68,23 @@ def twenty_four_times():
 @app.route("/twelve_times")
 def twelve_times():
     now = datetime.now()
-    current_time_12 = now.strftime("%I:%M:%S %p")
+    current_time_12 = now.strftime("%I:%M:%S")
     all_times["EST"] = current_time_12
+
+    gmt_hour = (int(current_time_12.split(":")[0]) + CONST_EST_GMT_DIFF) % HOUR_COUNT_ONE
+    minute = str(current_time_12.split(":")[1])
+    second = str(current_time_12.split(":")[2])
+
+    count = 0
+    for key in all_times:
+        final_time = str((gmt_hour + count) % HOUR_COUNT_TWO) + ":" + minute + ":" + second
+
+        if DAYLIGHT_SAVINGS:
+            if key.__eq__("PST") | key.__eq__("MST") | key.__eq__("CST") | key.__eq__("EST"):
+                final_time = str((gmt_hour + count + 1) % HOUR_COUNT_ONE) + ":" + minute + ":" + second
+
+        all_times[key] = final_time
+        count += 1
 
     return jsonify(all_times)
 
